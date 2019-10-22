782cff3006d7e1caf744ba2e69feb48a3e4ae828
debughao2333 <1528129383@qq.com>
2019-10-22 19:42:48 +0800

    01
    环境搭建

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,18 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from 'vue-router'
+import movieRouter from './movie'
+import mineRouter from './mine'
+import cinemaRouter from './cinema'
+Vue.use(Router)
 
-Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
+export default  new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes:[
+    movieRouter,
+    mineRouter,
+    cinemaRouter
+  ]
 })
 
-export default router
+
---
+++ src/views/Movie/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>小黄网</h1>
+    </div>    
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style >
+</style>
